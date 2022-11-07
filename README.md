429dede44ceb51d3dfbc9e8cb5f4cf0be4a4c9f9
santibenson <bensonsibigamisaac@gmail.com>
2022-11-07 04:05:52 +0100

    version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # rock-scissors-paper
- The popular rock, scissors and paper game.
+    The popular rock, scissors and paper game.
+
+# instructions
+    Rock wins against scissors; 
+    paper wins against rock; 
+    and scissors wins against paper. 
+    If both players throw the same hand signal, it is considered a tie, 
+    and play resumes until there is a clear winner.
---
+++ index.py
@@ -0,0 +1,50 @@
+# -------------- import a random module for computer to choose
+import random
+
+# -------------- create a random choice between rock, scissors and paper
+while True:
+    choices = ["rock", "scissors", "paper"]
+
+    computer = random.choice(choices)
+    player = None
+    player_round = 3
+    played = 0
+
+
+#---------- starts all over if the player's choice isn't in choices   
+    
+    while player not in choices:
+# -------------- capture the players input
+        player = input(f"rock, scissors or paper \n").lower()
+            
+        if player not in choices:
+            print("Not in choices. Try again")
+            
+        if player == computer:
+            print("Tie")
+
+        elif player == "rock":
+            if computer == "scissors":
+                print("You win")
+            elif computer == "paper":
+                print("You lose")
+
+        elif player == "scissors":
+            if computer == "rock":
+                    print("You lose")
+            elif computer == "paper":
+                    print("You win")
+
+        elif player == "paper":
+            if computer == "rock":
+                print("You win")
+            elif computer == "scissors":
+                  print("You lose")
+          
+    print("-----------GAME OVER!---------------------")
+    print("Do you want to play again? (yes/no)")
+#------------ decision to start the game again or not.
+    decision = input("> ").lower()
+    if decision != "yes":
+            break
+print("Press the run button to play again")
